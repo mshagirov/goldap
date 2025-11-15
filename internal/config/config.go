db64dfc73370cd720552ae1551b92b7d724d1397
Murat Shagirov <muratshagirov@gmail.com>
2025-11-15 23:49:09 +0800

    configuration file reading and writing

---
+++ internal/config/config.go
@@ -0,0 +1,97 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	LdapUrl     string `json:"LDAP_URL"`
+	LdapBaseDn  string `json:"LDAP_BASE_DN"`
+	LdapAdminDn string `json:"LDAP_ADMIN_DN"`
+}
+
+const fileName = ".goldapconfig.json"
+
+func getConfigPath() (string, error) {
+	HomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("Error getting config path: %v", err)
+	}
+	return HomeDir + "/" + fileName, nil
+}
+
+func ExampleJson() string {
+	configPath, err := getConfigPath()
+	if err != nil {
+		fmt.Println(err)
+	}
+	exampleJson := `{
+  "LDAP_URL":"ldap://localhost:389",
+  "LDAP_BASE_DN":"dc=goldap,dc=sh",
+  "LDAP_ADMIN_DN":"cn=admin,dc=goldap,dc=sh"
+}`
+	return fmt.Sprintf(`goldap config: LdapUrl is empty
+
+Create a JSON configuration file in:
+  %s
+Example configuration file contents:
+%s
+`, configPath, exampleJson)
+}
+
+func write(c Config) error {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+	jsonData, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(configPath, jsonData, 0666); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Read() Config {
+	var config Config
+	configPath, err := getConfigPath()
+	if err != nil {
+		return config
+	}
+	contents, err := os.ReadFile(configPath)
+	if err != nil {
+		return config
+	}
+	if err := json.Unmarshal(contents, &config); err != nil {
+		return Config{}
+	}
+	return config
+}
+
+func (c Config) SetUrl(ldapUrl string) error {
+	c.LdapUrl = ldapUrl
+	if err := write(c); err != nil {
+		return fmt.Errorf("Error writing config; %v", err)
+	}
+	return nil
+}
+
+func (c Config) SetBaseDn(baseDn string) error {
+	c.LdapBaseDn = baseDn
+	if err := write(c); err != nil {
+		return fmt.Errorf("Error writing config; %v", err)
+	}
+	return nil
+}
+
+func (c Config) SetlAdminDn(adminDn string) error {
+	c.LdapAdminDn = adminDn
+	if err := write(c); err != nil {
+		return fmt.Errorf("Error writing config; %v", err)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/mshagirov/goldap/internal/config"
 )
 
 func main() {
-	p := NewInitialModel()
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+	ldapConfig := config.Read()
+	if ldapConfig.LdapUrl == "" {
+		fmt.Printf("%v", config.ExampleJson())
 		os.Exit(1)
 	}
+
+	fmt.Printf("%#v\n", ldapConfig)
+	// p := NewInitialModel()
+	// if _, err := p.Run(); err != nil {
+	// 	fmt.Printf("Alas, there's been an error: %v", err)
+	// 	os.Exit(1)
+	// }
 }
