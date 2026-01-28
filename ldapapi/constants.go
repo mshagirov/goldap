9057d7da8c1be9cd8e38094bd7d7a0f65209efb1
Murat Shagirov <muratshagirov@gmail.com>
2026-01-28 10:02:21 +0800

    cleaned up file names in ldapapi package

--- ldapapi/constants.go
+++ ldapapi/constants.go
@@ -3,3 +3,44 @@ package ldapapi
 const (
 	ValueDelimeter = ", "
 )
+
+var (
+	TableNames = []string{
+		"Users",
+		"Groups",
+		"OrgUnits",
+	} // must match cases in *ldapapi.GetTableInfo(s string)
+
+	TableFilters = map[string]string{
+		"Users":    "(objectClass=PosixAccount)",
+		"Groups":   "(objectClass=PosixGroup)",
+		"OrgUnits": "(objectClass=OrganizationalUnit)",
+	}
+
+	UsrCols = []string{"Username", "uid", "Name", "Group"}
+	UsrAttr = map[string]string{
+		"uid":       "Username",
+		"uidNumber": "uid",
+		"cn":        "Name",
+		"gidNumber": "Group",
+	}
+	UsrColsWidth = []int{15, 5, 20, 25}
+
+	GrpCols = []string{"Name", "gid", "Members", "Description"}
+	GrpAttr = map[string]string{
+		"cn":          "Name",
+		"gidNumber":   "gid",
+		"memberUid":   "Members",
+		"member":      "Members",
+		"description": "Description",
+	}
+	GrpColsWidth = []int{15, 5, 30, 30}
+
+	OUCols = []string{"Name", "dn", "Description"}
+	OUAttr = map[string]string{
+		"ou":          "Name",
+		"dn":          "dn",
+		"description": "Description",
+	}
+	OUColsWidth = []int{15, 25, 25}
+)
--- ldapapi/parse.go
+++ ldapapi/parse.go
@@ -1,7 +1,11 @@
 package ldapapi
 
 import (
+	"fmt"
+	"slices"
 	"strings"
+
+	"github.com/go-ldap/ldap/v3"
 )
 
 func GetFirstDnAttr(dn string) (attr, value string, ok bool) {
@@ -28,4 +32,29 @@ func GetFirstDnAttrs(dns []string) (attrs, values []string, ok bool) {
 		}
 	}
 	return attrs, values, true
+}
+
+func GetUsersColId(attr string) int {
+	colName, ok := UsrAttr[attr]
+	gidColId := -1
+	if ok {
+		gidColId = slices.Index(UsrCols, colName)
+	}
+	return gidColId
+}
+
+func FormatRDNFilter(tableFilter, rdn string) string {
+	return fmt.Sprintf("(&%s(%s))", tableFilter, ldap.EscapeFilter(rdn))
+}
+
+func FormatDNFilter(dn, tableName string) string {
+	rdn, _, found := strings.Cut(dn, ",")
+	if !found {
+		return ""
+	}
+	tableFilter, ok := TableFilters[tableName]
+	if !ok {
+		return fmt.Sprintf("(%s)", ldap.EscapeFilter(rdn))
+	}
+	return FormatRDNFilter(tableFilter, rdn)
 }
